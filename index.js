e0f5b5bf775994bf13a655c181cbf8632dbad73c
Ryan Seit <ryan@ryanseit.com>
2020-03-29 14:17:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if (someValue <= 400) {
+    return 'This one is on me!';
+  } else if (someValue >= 2500) {
+    return 'No can do.';
+  } else if (someValue >= 2000) {
+    return 'I will gladly take your thirty bucks.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityName){
   // Write your code here!
+  const driverResponse = cityName === "NYC" ? 'Ok, sounds good.' : 'No go.';
+  return driverResponse;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    case 'thanks for everything':
+      return 'Bye.';
+  }
 }
